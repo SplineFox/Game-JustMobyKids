205bd062318d471b7df30542ea5f3f2823633e51
SplineFox <betaspfox@gmail.com>
2025-07-29 21:46:44 +0900

    Add LocalizationController to initialize and track localization

--- Assets/Scripts/Localizations/ILocalizationService.cs
+++ Assets/Scripts/Localizations/ILocalizationService.cs
@@ -1,7 +1,12 @@
+using System;
+
 public interface ILocalizationService
 {
+    event Action LocaleChanged;
+    
     string CurrentLocaleCode { get; }
     
+    void Initialize(string localeCode);
     void SetLocaleCode(string localeCode);
     string GetText(string id);
     bool HasText(string id);

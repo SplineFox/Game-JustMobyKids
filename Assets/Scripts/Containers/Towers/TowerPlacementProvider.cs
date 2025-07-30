64c18ef0dc1ef65df42356facfb241a6023c99e2
SplineFox <betaspfox@gmail.com>
2025-07-30 10:26:50 +0900

    Fix position clamping

--- Assets/Scripts/Containers/Towers/TowerPlacementProvider.cs
+++ Assets/Scripts/Containers/Towers/TowerPlacementProvider.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TowerPlacementProvider : ITowerPlacementProvider
 {
+    private const float PlacementThreshold = 0.25f;
+    
     public Vector3 GetPositionForElement(IReadOnlyList<Element> elements, Element dropElement, Vector2 dropPosition, RectTransform towerRect)
     {
         if (elements.Count == 0)
@@ -19,12 +21,13 @@ public class TowerPlacementProvider : ITowerPlacementProvider
             out var localPosition
         );
 
-        var widthThreshold = towerRect.rect.width * 0.25f;
         var bottomY = - towerRect.rect.height * 0.5f;
 
-        var posX = Mathf.Clamp(localPosition.x, -widthThreshold, widthThreshold);
+        var posX = localPosition.x;
         var posY = bottomY + dropElement.RectTransform.rect.height * 0.5f;
         var posZ = dropElement.RectTransform.position.z;
+        
+        posX = ClampPositionX(posX, towerRect);
         var pos = new Vector3(posX, posY, posZ);
 
         return towerRect.TransformPoint(pos);
@@ -36,13 +39,21 @@ public class TowerPlacementProvider : ITowerPlacementProvider
         
         var height = dropElement.RectTransform.rect.height;
         var width = dropElement.RectTransform.rect.height;
-
+        
         var posXOffset = Random.Range(-1f, 1f) * (width * 0.5f);
         var posX = lastElement.RectTransform.localPosition.x + posXOffset;
         var posY = lastElement.RectTransform.localPosition.y + (lastHeight + height) * 0.5f;
         var posZ = lastElement.RectTransform.localPosition.z;
+
+        posX = ClampPositionX(posX, towerRect);
         var pos = new Vector3(posX, posY, posZ);
         
         return towerRect.TransformPoint(pos);
+    }
+
+    private float ClampPositionX(float value, RectTransform towerRect)
+    {
+        var widthThreshold = towerRect.rect.width * PlacementThreshold;
+        return Mathf.Clamp(value, -widthThreshold, widthThreshold);
     }
 }
